8e2ef11335db728f8325631ccc4c678bfaffd041
ofirAvisror <ofir071002@gmail.com>
2025-08-28 11:38:19 +0300

    LOGO

--- Server/config/db.ts
+++ Server/config/db.ts
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/burgerApp";
+  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Grabbit";
 
   try {
     await mongoose.connect(uri, {
